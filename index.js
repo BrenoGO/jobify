6b1bdb99dd773d8c1410314383775a04ffc30fe0
Breno Oliveira <breno.oliveira.ufv@gmail.com>
2019-04-08 14:23:21 -0300

    Added port as env variable

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -89,7 +90,7 @@ const init = async() => {
     // await db.run(`insert into vagas (categoria, titulo, descricao) values (2, '${titulo}', '${descricao}')`)
 }
 init();
-app.listen(3000, err => {
+app.listen(port, err => {
     if(err) {
         console.log('Não foi possível iniciar o servidor do Jobify')
     } else {
